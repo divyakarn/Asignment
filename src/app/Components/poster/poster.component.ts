3ebeb02ed6b194aa305eaa3817f0fc8427c0d6a3
divyakarnshukla <divyakarns@spryhealth.care>
2024-06-10 14:22:01 +0530

    added forms logic

--- src/app/Components/poster/poster.component.ts
+++ src/app/Components/poster/poster.component.ts
@@ -1,23 +1,54 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-
+import { ApiService } from '../../Services/api.service.';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 @Component({
   selector: 'app-poster',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './poster.component.html',
-  styleUrl: './poster.component.scss'
+  styleUrl: './poster.component.scss',
+  providers:[ApiService]
 })
 export class PosterComponent {
-  constructor(private route: ActivatedRoute) {}
+  posterForm: FormGroup;
+  constructor(private route: ActivatedRoute,private apiService: ApiService,private fb: FormBuilder) {
+    this.posterForm = this.fb.group({
+      state: [''],
+      city: [''],
+      area: [''],
+      rent: ['']
+    });
+  }
   inputValue:any;
+
 
   ngOnInit() {
     this.route.queryParams.subscribe(params => {
         // Access the input parameter passed from the previous component
         this.inputValue = params['input'];
-        alert( this.inputValue);
     });
+    
+}
+
+
+onSubmit(action: string) {
+  if (action === 'post') {
+    this.postData();
+  } else if (action === 'update') {
+    this.updateData();
+  }
+}
+
+postData() {
+  // Handle post action
+  console.log('Posting data:', this.posterForm.value);
+}
+
+updateData() {
+  // Handle update action
+  console.log('Updating data:', this.posterForm.value);
 }
   
 
--- src/app/Components/seeker/seeker.component.ts
+++ src/app/Components/seeker/seeker.component.ts
@@ -1,24 +1,31 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ApiService } from '../../Services/api.service.';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-seeker',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './seeker.component.html',
   styleUrl: './seeker.component.scss',
-  providers:[ApiService]
+  providers:[ApiService,ReactiveFormsModule]
 })
 export class SeekerComponent {
-  constructor(private route: ActivatedRoute ,private apiService:ApiService) {}
+  seekerForm: FormGroup;
+  constructor(private route: ActivatedRoute ,private apiService:ApiService,private fb: FormBuilder) {
+    this.seekerForm = this.fb.group({
+      state: [''],
+      city: [''],
+      area: ['']
+    });
+  }
   inputValue:any;
 
   ngOnInit() {
     this.route.queryParams.subscribe(params => {
         // Access the input parameter passed from the previous component
         this.inputValue = params['input'];
-        alert(this.inputValue);
     });
 
     const postData = { title: 'New Post', body: 'This is a new post.' };
@@ -26,4 +33,10 @@ export class SeekerComponent {
       console.log('Response from POST request:', response);
     });
 }
+onSubmit() {
+  // Process the form data
+  console.log(this.seekerForm.value);
+}
+
+
 }
